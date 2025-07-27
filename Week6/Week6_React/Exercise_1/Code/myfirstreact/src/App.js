2d00fe1c39f89becd7a392ae4f54e1b5e6e79373
Antar Mukhopadhyaya <mukhopadhyayaantar@gmail.com>
2025-07-27 13:53:09 +0530

    feat: completed exercise 1 of week 6 with output and readme update

---
+++ Week6/Week6_React/Exercise_1/Code/myfirstreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+
+            <h1>Welcome to the first exercise of React <strong><i>By Antar Mukhopadhyaya</i></strong></h1>
+
+    </div>
+  );
+}
+
+export default App;
